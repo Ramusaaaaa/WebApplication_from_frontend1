b44cfc61c345d7374b41e8830d5009577f9a383c
ramusaaaaa <bilim8968@gmail.com>
2023-07-31 11:25:48 +0300

    hmm

---
+++ src/models/product.ts
@@ -0,0 +1,13 @@
+export class Product {
+    constructor(
+
+            id: number,
+            title: string,
+            description: string,
+            image: string,
+            price: number
+
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+    ) {
+    }
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,40 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+import Dashboard from '@/pages/DashboardPage.vue';
+import Register from '@/pages/RegisterPage.vue';
+import Wrapper from '@/pages/WrapperPage.vue';
+import Login from '@/pages/LoginPage.vue';
+import Users from '@/pages/users/UsersPage.vue';
+import UserCreate from '@/pages/users/UserCreate.vue';
+import UserEdit from '@/pages/users/UserEdit.vue';
+import Roles from '@/pages/roles/RolesPage.vue';
+import RoleCreate from '@/pages/roles/RoleCreate.vue';
+import RoleEdit from '@/pages/roles/RoleEdit.vue';
+import Products from '@/pages/products/ProductsPage.vue';
+import ProductCreate from '@/pages/products/ProductCreate.vue';
+import ProductEdit from '@/pages/products/ProductEdit.vue';
+import Orders from '@/pages/orders/OrdersPage.vue';
+import Profile from '@/pages/ProfilePage.vue';
 
 const routes: Array<RouteRecordRaw> = [
+  {path: '/register', component: Register},
+  {path: '/login', component: Login},
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '',
+    component: Wrapper,
+    children: [
+      {path: '', component: Dashboard},
+      {path: '/profile', component: Profile},
+      {path: '/users', component: Users},
+      {path: '/users/create', component: UserCreate},
+      {path: '/users/:id/edit', component: UserEdit},
+      {path: '/roles', component: Roles},
+      {path: '/roles/create', component: RoleCreate},
+      {path: '/roles/:id/edit', component: RoleEdit},
+      {path: '/products', component: Products},
+      {path: '/products/create', component: ProductCreate},
+      {path: '/products/:id/edit', component: ProductEdit},
+      {path: '/orders', component: Orders},
+    ]
   }
 ]
 
